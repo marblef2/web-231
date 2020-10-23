d512b5625ea59a73e6d2f737d3407033e86b542d
Fred Marble <fmarble@my365.bellevue.edu>
2020-10-23 17:49:36 -0500

    Add files via upload

--- week-1/hello-world.js
+++ week-1/hello-world.js
@@ -1,1 +1,10 @@
+/*
+============================================
+; Title: Exercise 1.3 - Hello World
+; Author: Fred Marble
+; Date: 23 October 2020
+; Description: This program prints out Hello World, my name is Fred Marble.
+;===========================================
+*/
+
 console.log("Hello World, my name is Fred Marble");
