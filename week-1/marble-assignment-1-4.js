dc4dd1f2c704e1b11ebef66dca59595dfb0bb4ca
Fred Marble <fmarble@my365.bellevue.edu>
2020-10-24 15:48:28 -0500

    Adding Assignment 1.4

---
+++ week-1/marble-assignment-1-4.js
@@ -0,0 +1,25 @@
+/*
+============================================
+; Title:  marble-assignment-1-4.js
+; Author: Fred Marble
+; Modified By:
+; Date:   24 October 2020
+; Description: Demonstrates how to assign and output variable values
+;===========================================
+*/
+
+// variables
+const firstName = 'Fred'
+const lastName = 'Marble'
+const age = '29'
+const street = '125 Highfalutton Ave'
+const zipCode = '8675309'
+const city = 'Denver'
+
+// output
+console.log('First Name: ' + firstName)
+console.log('Last Name: ' + lastName)
+console.log('Age: ' + age)
+console.log('Street Address: ' + street)
+console.log('City: ' + city)
+console.log('Zip Code: ' + zipCode)
