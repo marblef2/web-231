2fa490982964857c427f24e6b3bbbf7ba89bfb74
Fred Marble <fmarble@my365.bellevue.edu>
2020-10-25 09:26:33 -0500

    Adding Assignment 1.5.js and apparently merging assignment 1.4

---
+++ week-1/marble-assignment-1-5.js
@@ -0,0 +1,46 @@
+/*
+============================================
+; Title:  marble-assignment-1-5.js
+; Author: Fred Marble
+; Modified By:
+; Date:   25 October 2020
+; Description: Demonstrates how to assign and output variable values
+;===========================================
+*/
+
+// variables
+const firstName = 'Fred'
+const lastName = 'Marble'
+const address = '125 Highfalutton Ave Denver, CO  86753'
+const payRate = '8675.3'
+const hireDate = '12/10/20'
+const firstName2 = 'Ducan'
+const lastName2 = 'Von Hausen'
+const address2 = '125 Bakers St Kearney, NE 55152'
+const payRate2 = '67530.9'
+const hireDate2 = '01/10/14'
+const firstName3 = 'Duchess'
+const lastName3 = 'Rocker'
+const address3 = '125 Bleaker St Denver, CO  86753'
+const payRate3 = '1245.8'
+const hireDate3 = '01/25/10'
+
+
+// output
+console.log('First Name: ' + firstName)
+console.log('Last Name: ' + lastName)
+console.log('Address: ' + address)
+console.log('Pay Rate: ' + payRate)
+console.log('Hire Date: ' + hireDate)
+console.log('')
+console.log('First Name: ' + firstName2)
+console.log('Last Name: ' + lastName2)
+console.log('Address: ' + address2)
+console.log('Pay Rate: ' + payRate2)
+console.log('Hire Date: ' + hireDate2)
+console.log('')
+console.log('First Name: ' + firstName3)
+console.log('Last Name: ' + lastName3)
+console.log('Address: ' + address3)
+console.log('Pay Rate: ' + payRate3)
+console.log('Hire Date: ' + hireDate3)
