8520aff79d30337066d451b0386df8929dec5dde
Fred Marble <fmarble@my365.bellevue.edu>
2020-10-29 15:32:05 -0500

    Adding Exercise 2.2 to Week 2.

---
+++ week-2/marble-header-test.js
@@ -0,0 +1,14 @@
+/*
+============================================
+; Title:  marble-header-test.js
+; Author: Fred Marble
+; Date:   29 October 2020
+; Description: Displays a formatted header
+;===========================================
+*/
+
+//Links the Header file to the project.
+const header = require('./marble-header.js')
+
+//Displays the Header.
+console.log(header.display("Fred", "Marble", "Exercise 2.2"));
