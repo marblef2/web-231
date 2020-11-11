773c6b9dd8d0cb07582fa845a1ea66452271fd27
Fred Marble <fmarble@my365.bellevue.edu>
2020-11-11 15:18:34 -0600

    Adding the Week-4 folder as well as exercise 4.2

---
+++ week-4/marble-exercise-4.2.js
@@ -0,0 +1,46 @@
+/*
+============================================
+; Title:  marble-exercise-4.2-arrays.js
+; Author: Professor Krasso
+; Modified By: Fred Marble
+; Date:  11 November 2020
+; Description: A one dimensional array for fruit.
+;===========================================
+*/
+
+//Links the Header file to the project.
+const header = require('./marble-header.js')
+
+// Creating the Fruit Array.
+
+const fruits = ["Apple", "Orange", "Banana", "Pear", "Mango"];
+
+// Creation of the getFruits function, so we can display it.
+function getFruits(arr){
+  for (f=0; f< arr.length; f++){
+    console.log(arr[f]);
+  }
+}
+
+/*
+  Expected output:
+
+  FirstName LastName
+  Exercise 4.2
+  Today's Date
+
+  Apple
+  Orange
+  Banana
+  Mango
+  Pear
+*/
+
+//Displays the Header.
+console.log(header.display("Fred", "Marble", "Exercise 4.2"));
+
+//Places a blank space between header & exercise results.
+console.log("\n")
+
+//Calling the getFruits Function to show the fruit in the array.
+getFruits(fruits);
