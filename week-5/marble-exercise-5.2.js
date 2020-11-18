a15b578ae6d88b798f8918eaff6675375c184fd6
Fred Marble <fmarble@my365.bellevue.edu>
2020-11-18 14:41:48 -0600

    Adding the Week 5 Folder, and Exercise 5.2

---
+++ week-5/marble-exercise-5.2.js
@@ -0,0 +1,26 @@
+/*
+============================================
+; Title: marble-exercise-5.2.js
+; Author: Professor Krasso
+; Date:   18 November 2020
+; Modified By: Fred Marble
+; Description: Displays a formatted header
+;===========================================
+*/
+
+//Linking the header file.
+const header = require('./marble-header.js')
+
+//Creating the Array with my favorite foods.
+let foods = ["Pizza", "Steak", "Mashed Potato", "Meatloaf", "Porkchops"];
+
+//Displays the Header.
+console.log(header.display("Fred", "Marble", "Exercise 5.2"));
+
+//Adding a blank space between the header and exercise content.
+console.log("");
+
+//Using the forEach() function to go over the array.
+foods.forEach(function (food){
+  console.log(food);
+});
